65436b7ce55213c2e3b4ac163c8c16770c341eaf
sudshanb <sudshanb@IN-MUM-LDVM121.corp.capgemini.com>
2018-09-12 01:29:23 -0400

    Accounts

--- src/com/capgemini/idbibankapp/controller/AccountController.java
+++ src/com/capgemini/idbibankapp/controller/AccountController.java
@@ -7,24 +7,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AccountController
- */
+
 @WebServlet("/account")
 public class AccountController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
     public AccountController() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
