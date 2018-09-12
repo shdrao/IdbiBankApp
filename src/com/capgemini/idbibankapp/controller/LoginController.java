2e8a1c29f376e244b751614bc14014ff67b163b0
harink <harink@IN-MUM-LDVM039.corp.capgemini.com>
2018-09-12 11:00:49 +0530

    Harini's Login controller

--- src/com/capgemini/idbibankapp/controller/LoginController.java
+++ src/com/capgemini/idbibankapp/controller/LoginController.java
@@ -1,22 +1,41 @@
 package com.capgemini.idbibankapp.controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.capgemini.idbibankapp.dao.CustomerDao;
+import com.capgemini.idbibankapp.dao.impl.CustomerDaoImpl;
+
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private CustomerDao customerDao;
+	private ServletContext context;
 
-    public LoginController() {
-        // TODO Auto-generated constructor stub
-    }
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		context = config.getServletContext();
+	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public LoginController() {
+		super();
+		customerDao = new CustomerDaoImpl();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		
+		
+
 	}
 
 }
