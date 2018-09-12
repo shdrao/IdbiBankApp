9488a1fc7be6a10b1be23a48d4a1df4bd827eac5
sudshanb <sudshanb@IN-MUM-LDVM121.corp.capgemini.com>
2018-09-12 01:29:42 -0400

    Accounts

--- src/com/capgemini/idbibankapp/controller/AccountController.java
+++ src/com/capgemini/idbibankapp/controller/AccountController.java
@@ -19,8 +19,7 @@ public class AccountController extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 }
