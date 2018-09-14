b36b8afcb8ed5f87c999098d21bb803620830c61
shdrao <shdrao@IN-MUM-LDVM038.corp.capgemini.com>
2018-09-13 23:43:13 -0400

    fsf

--- src/com/capgemini/idbibankapp/filter/CookieEnableFilter.java
+++ src/com/capgemini/idbibankapp/filter/CookieEnableFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 
 /**
  * Servlet Filter implementation class CookieEnableFilter
@@ -33,10 +34,8 @@ public class CookieEnableFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
-		// pass the request along the filter chain
+		response.setContentType("text/html");
+		//Cookie [] cookies=request.
 		chain.doFilter(request, response);
 	}
 
